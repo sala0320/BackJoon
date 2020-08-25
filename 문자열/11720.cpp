0d4436fd1d2826c5a88620426af13b3d2793f74f
sala0320 <sala0320@naver.com>
2020-08-25 19:47:22 +0900

    11720 push

---
+++ 문자열/11720.cpp
@@ -0,0 +1,22 @@
+#include <iostream> 
+using namespace std;
+int main()
+{
+	int num;
+	char input;
+	int sum = 0;
+
+	cin >> num;
+
+	for (int i = 0; i < num; i++)
+	{
+		cin >> input;
+		sum += (input - 48);
+	}
+	cout << sum;
+}
+
+
+
+
+
