395f46792d82f516b8f0cfeb36911e760eb3edd1
sala0320 <sala0320@naver.com>
2020-08-26 01:11:11 +0900

    push

---
+++ 문자열/1152.cpp
@@ -0,0 +1,31 @@
+//isspace: 공백유무 함수 이용하기
+
+#include <iostream> 
+#include <string>
+#define MAX 1000000
+using namespace std;
+int main()
+{
+	string str;
+	int len, count = 0;
+
+	getline(cin, str);
+	len = str.length();
+
+	for (int i = 0; i < len; i++)
+	{
+		if (str[i] == ' ')
+			count++;
+	}
+	if (isspace(str[0]))
+		count--;
+	if (isspace(str[len - 1]))
+		count--;
+
+	cout << count + 1;
+}
+
+
+
+
+
