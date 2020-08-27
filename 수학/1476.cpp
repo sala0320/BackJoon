7f0622beacb3d3a68a4021a3a61781c26a919e28
sala0320 <sala0320@naver.com>
2020-08-28 00:44:54 +0900

    math push

---
+++ 수학/1476.cpp
@@ -0,0 +1,22 @@
+//
+#include <iostream>
+using namespace std;
+int main()
+{
+	int E, S, M;
+	int year = 1;
+	cin >> E >> S >> M;
+
+	while (1)
+	{
+		if ((year - E) % 15 == 0 && (year - S) % 28 == 0 && (year - M) % 19 == 0)
+		{
+			cout << year;
+			break;
+		}
+		year++;
+	}
+}
+
+
+
